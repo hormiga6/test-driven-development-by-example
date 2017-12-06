f53226ca4a86307a041a213e27f594ea6d7848ed
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-12-06 12:45:23 +0900

    chapter 4

--- src/test/money/MoneyTest.java
+++ src/test/money/MoneyTest.java
@@ -8,10 +8,8 @@ class MoneyTest {
     @Test
     public void testMultiplication(){
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10,product.amount);
-        product = five.times(3);
-        assertEquals(15,product.amount);
+        assertEquals(new Dollar(10),five.times(2));
+        assertEquals(new Dollar(15),five.times(3));
     }
 
     @Test
@@ -19,5 +17,4 @@ class MoneyTest {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
-
 }
