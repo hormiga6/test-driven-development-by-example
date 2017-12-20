c58478de92de7454c49eb2146ee381cef06f9aa5
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-12-20 13:18:31 +0900

    chapter 12

--- src/main/money/Money.java
+++ src/main/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-class Money {
+class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -10,8 +10,13 @@ class Money {
     }
 
     Money times(int multiplier){
-        return null;
+        return new Money(amount * multiplier, currency);
     }
+
+    Expression plus(Money addend){
+        return new Money(amount + addend.amount, currency);
+    }
+
     String currency(){
         return currency;
     }
